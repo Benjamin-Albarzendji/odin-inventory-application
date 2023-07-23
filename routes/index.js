ac6df357b28c1a499cba68ef2dd7176e234dfedc
Benjamin-Albarzendji <105798796+Benjamin-Albarzendji@users.noreply.github.com>
2023-07-23 21:21:39 +0200

    Route established

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const inventoryController = require('../controllers/inventoryController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', inventoryController.index);
+
+// GET request for creating a Inventory. NOTE This must come before routes that display Inventory (uses id).
+router.get('/inventory/create', inventoryController.inventory_create_get);
+
+// POST request for creating item.
+router.post('/inventory/create', inventoryController.inventory_create_post);
+
+// GET request to delete item.
+router.get('/inventory/:id/delete', inventoryController.inventory_delete_get);
+
+// POST request to delete item.
+router.post('/inventory/:id/delete', inventoryController.inventory_delete_post);
+
+// GET request to update item.
+router.get('/inventory/:id/update', inventoryController.inventory_update_get);
+
+// POST request to update item.
+router.post('/inventory/:id/update', inventoryController.inventory_update_post);
+
+// GET request for one item.
+router.get('/inventory/:id', inventoryController.inventory_detail);
+
+// GET request for list of all item.
+router.get('/inventories', inventoryController.inventory_list);
+// GET request for list of all categories.
+router.get('categories', inventoryController.category_list);
+
+// GET request for one category.
+router.get('/category/:id', inventoryController.category_detail);
 
 module.exports = router;
