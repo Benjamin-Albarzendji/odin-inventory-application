97f8d572679d0af27acb09029911e0cfe76186b7
Benjamin-Albarzendji <105798796+Benjamin-Albarzendji@users.noreply.github.com>
2023-07-28 20:37:26 +0200

    Created views and routes

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -18,6 +18,7 @@ exports.index = asyncHandler(async (req, res, next) => {
   });
 });
 
+// Inventory create on GET.
 exports.inventory_create_get = asyncHandler(async (req, res, next) => {
   const categories = await Category.find({}).exec();
 
@@ -27,6 +28,7 @@ exports.inventory_create_get = asyncHandler(async (req, res, next) => {
   });
 });
 
+//  Inventory create on POST.
 exports.inventory_create_post = [
   // Validate and sanitize fields.
   body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
@@ -71,6 +73,150 @@ exports.inventory_create_post = [
       // Data from form is valid. Save item.
       await item.save();
       // Successful - redirect to new item record.
+      res.redirect('/');
+    }
+  }),
+];
+// Get the inventory detail
+exports.inventory_detail = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate('category').exec();
+
+  if (item == null) {
+    const err = new Error('Item not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('inventory_detail', {
+    title: 'Item Detail',
+    item,
+  });
+});
+
+// Get the inventory list
+exports.inventory_list = asyncHandler(async (req, res, next) => {
+  const items = await Item.find({}).populate('category').exec();
+
+  res.render('inventory_list', {
+    title: 'Inventory List',
+    items,
+  });
+});
+// Get the category list
+exports.category_list = asyncHandler(async (req, res, next) => {
+  const categories = await Category.find({}).exec();
+
+  res.render('category_list', {
+    title: 'Category List',
+    categories,
+  });
+});
+
+// Get the category detail items
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const category = await Category.findById(req.params.id).exec();
+
+  if (category == null) {
+    const err = new Error('Category not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  const items = await Item.find({ category: req.params.id }).exec();
+
+  res.render('category_detail', {
+    title: 'Category Detail',
+    category,
+    items,
+  });
+});
+
+// Get the inventory delete on GET.
+exports.inventory_delete_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).exec();
+
+  if (item == null) {
+    res.redirect('/inventory');
+  }
+
+  res.render('inventory_delete', {
+    title: 'Delete Item',
+    item,
+  });
+});
+
+// Get the inventory delete on POST.
+exports.inventory_delete_post = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.itemid).exec();
+
+  if (item == null) {
+    res.redirect('/inventory');
+  }
+
+  await Item.findByIdAndRemove(req.body.itemid);
+
+  res.redirect('/inventory');
+});
+
+// Get the inventory update on GET.
+exports.inventory_update_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).exec();
+  const categories = await Category.find({}).exec();
+
+  if (item == null) {
+    res.redirect('/inventory');
+  }
+
+  res.render('inventory_form', {
+    title: 'Update Item',
+    item,
+    categories,
+  });
+});
+
+// Get the inventory update on POST.
+exports.inventory_update_post = [
+  // Validate and sanitize fields.
+  body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Description must not be empty.').trim(),
+  body('price', 'Price must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('stock', 'Stock must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract errors from a request.
+    const errors = validationResult(req);
+
+    // Create an Item object with escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+      const categories = await Category.find({}).exec();
+
+      res.render('inventory_form', {
+        title: 'Update Item',
+        categories,
+        item,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Update the record.
+      await Item.findByIdAndUpdate(req.params.id, item, {});
+      // Successful - redirect to item detail page.
       res.redirect('/');
     }
   }),
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const inventoryController = require('../controllers/inventoryController');
 /* GET home page. */
 router.get('/', inventoryController.index);
 
+// // GET request for list of all item.
+router.get('/inventory', inventoryController.inventory_list);
+
 // // GET request for creating a Inventory. NOTE This must come before routes that display Inventory (uses id).
 router.get('/inventory/create', inventoryController.inventory_create_get);
 
@@ -14,26 +17,24 @@ router.get('/inventory/create', inventoryController.inventory_create_get);
 router.post('/inventory/create', inventoryController.inventory_create_post);
 
 // // GET request to delete item.
-// router.get('/inventory/:id/delete', inventoryController.inventory_delete_get);
+router.get('/inventory/:id/delete', inventoryController.inventory_delete_get);
 
 // // POST request to delete item.
-// router.post('/inventory/:id/delete', inventoryController.inventory_delete_post);
+router.post('/inventory/:id/delete', inventoryController.inventory_delete_post);
 
 // // GET request to update item.
-// router.get('/inventory/:id/update', inventoryController.inventory_update_get);
+router.get('/inventory/:id/update', inventoryController.inventory_update_get);
 
 // // POST request to update item.
-// router.post('/inventory/:id/update', inventoryController.inventory_update_post);
+router.post('/inventory/:id/update', inventoryController.inventory_update_post);
 
 // // GET request for one item.
-// router.get('/inventory/:id', inventoryController.inventory_detail);
+router.get('/inventory/:id', inventoryController.inventory_detail);
 
-// // GET request for list of all item.
-// router.get('/inventories', inventoryController.inventory_list);
 // // GET request for list of all categories.
-// router.get('categories', inventoryController.category_list);
+router.get('/categories', inventoryController.category_list);
 
 // // GET request for one category.
-// router.get('/category/:id', inventoryController.category_detail);
+router.get('/category/:id', inventoryController.category_detail);
 
 module.exports = router;
